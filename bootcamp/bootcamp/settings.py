e901deb25e22919ed7fb8ec5e361af2a0518ff4d
gzpgg3x <gzpgg3x@yahoo.com>
2015-01-29 23:17:23 -0500

    initial
    initial

---
+++ bootcamp/bootcamp/settings.py
@@ -0,0 +1,121 @@
+from unipath import Path
+PROJECT_DIR = Path(__file__).parent
+
+from decouple import config
+
+import dj_database_url
+
+# import os
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+# SETTINGS_DIR = os.path.dirname(__file__)
+# PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
+# PROJECT_PATH = os.path.abspath(PROJECT_PATH)
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '79&vz)($@07na+25vw4nb0r^p*6w0j+-x!m)y5p#76tp!gvs_5'
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': dj_database_url.config(
+      default = config('DATABASE_URL'))
+}
+
+# DATABASE_PATH = os.path.join(PROJECT_PATH, 'database.db')
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': DATABASE_PATH,
+#     }
+# }
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    # 'south',
+    'bootcamp.activities',
+    'bootcamp.articles',
+    'bootcamp.auth',
+    'bootcamp.core',
+    'bootcamp.feeds',
+    'bootcamp.messages',
+    'bootcamp.questions',
+    'bootcamp.search',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'bootcamp.urls'
+
+WSGI_APPLICATION = 'bootcamp.wsgi.application'
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'America/New_York'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+LANGUAGES = (
+    ('en', 'English'),
+    ('pt-br', 'Portuguese'),
+    ('es', 'Spanish')
+)
+
+LOCALE_PATHS = (PROJECT_DIR.child('locale'), )
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_ROOT = PROJECT_DIR.parent.child('staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    PROJECT_DIR.child('static'),
+)
+
+MEDIA_ROOT = PROJECT_DIR.parent.child('media')
+MEDIA_URL = '/media/'
+
+TEMPLATE_DIRS = (
+    PROJECT_DIR.child('templates'),
+)
+
+LOGIN_URL = '/'
+LOGIN_REDIRECT_URL = '/feeds/'
+
+ALLOWED_SIGNUP_DOMAINS = ['*']
+
+FILE_UPLOAD_TEMP_DIR = '/tmp/'
+FILE_UPLOAD_PERMISSIONS = 0644
